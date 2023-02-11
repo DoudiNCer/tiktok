48676437417487d9e8eb91150666cde061988d33
DoudiNCer <doudi@outlook.lv>
2023-02-11 18:33:09 +0800

    fix: 统一数据库

--- biz/dal/sql/init.go
+++ biz/dal/sql/init.go
@@ -10,7 +10,7 @@ var DB *gorm.DB
 // InitDb gorm初始化函数
 func InitDb() error {
 	var err error
-	dsn := "root:asd020118@tcp(127.0.0.1:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "tiktok:tiktok@tcp(127.0.0.1:13306)/tiktok?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return err
