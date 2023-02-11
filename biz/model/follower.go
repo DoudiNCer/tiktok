4f4a3ea7e902fc1c83116a7044d8e18492d14f18
sterben <1473352497@qq.com>
2023-02-12 01:27:55 +0800

    message: - 配置数据库 - 导入mysqlDriver - 导入hertz相关依赖 - 完成关注操作接口

--- biz/model/follower.go
+++ biz/model/follower.go
@@ -1,12 +1,14 @@
 package model
 
+import "time"
+
 type Follower struct {
-	Id         int64  `json:"id" column:"id"`
-	ToUserUid  int64  `json:"to_user_uid" column:"to_user_uid"`
-	UserUid    int64  `json:"user_uid" column:"user_uid"`
-	CreateTime string `json:"create_time" column:"create_time"`
-	IsDelete   bool   `json:"is_delete" column:"is_delete"`
-	UpdateTime string `json:"update_time" column:"update_time"`
+	Id         int64     `json:"id" column:"id"`
+	ToUserUid  int64     `json:"to_user_uid" column:"to_user_uid"`
+	UserUid    int64     `json:"user_uid" column:"user_uid"`
+	CreateTime time.Time `json:"create_time" column:"create_time"`
+	IsDeleted  bool      `json:"is_delete" column:"is_delete"`
+	UpdateTime time.Time `json:"update_time" column:"update_time"`
 }
 
 func (f *Follower) TableName() string {
--- biz/model/user.go
+++ biz/model/user.go
@@ -1,10 +1,12 @@
 package model
 
+import "time"
+
 type User struct {
-	Id         int64  `json:"id" column:"id"`
-	Name       string `json:"name" column:"name"`
-	Password   string `json:"password" column:"password"`
-	CreateTime string `json:"create_time" column:"create_time"`
+	Id         int64     `json:"id" column:"id"`
+	Name       string    `json:"name" column:"name"`
+	Password   string    `json:"password" column:"password"`
+	CreateTime time.Time `json:"create_time" column:"create_time"`
 }
 
 func (u *User) TableName() string {
