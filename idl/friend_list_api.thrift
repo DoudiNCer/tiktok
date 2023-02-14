07bc12160760d0ebab558101ab97d2f2fe987883
DoudiNCer <doudi@outlook.lv>
2023-02-14 16:19:23 +0800

    feat: 添加获取好友列表接口

---
+++ idl/friend_list_api.thrift
@@ -0,0 +1,34 @@
+namespace go friend_list_gorm
+
+enum Code {
+     Success         = 0
+     ParamInvalid    = 1
+     DBErr           = 2
+     RTErr           = 3
+}
+
+struct GetFriendListRequest{
+    1: string user_id       (api.query="user_id")
+    2: string token         (api.query="token")
+}
+
+struct FriendUser{
+    i64 id // 用户ID
+    string name // 用户名
+    i64 follow_count // 关注总数
+    i64 follower_count // 粉丝总数
+    bool is_follow // true-已关注，false-未关注
+    string avatar // 用户头像Url
+    string message //  和该好友的最新聊天消息
+    i64 msgType //  message消息的类型，0 => 当前请求用户接收的消息， 1 => 当前请求用户发送的消息
+}
+
+struct GetFriendListResponse{
+    1: Code status_code
+    2: string status_msg
+    3: list<FriendUser> user_list
+}
+
+service FriendService{
+    GetFriendListResponse GetFriendList(1:GetFriendListRequest req)(api.get="/douyin/relation/friend/list/")
+}
