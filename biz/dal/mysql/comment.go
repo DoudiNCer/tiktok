579724eca7662c30b95f21e89fcc01be4ab2a75a
root <18960850393@163.com>
2023-02-17 12:09:32 +0800

    完成评论列表接口

--- biz/dal/mysql/comment.go
+++ biz/dal/mysql/comment.go
@@ -1,9 +1,11 @@
 package mysql
 
-import "github.com/DodiNCer/tiktok/biz/model"
+import (
+	"github.com/DodiNCer/tiktok/biz/model"
+)
 
 func QueryCommentByCommentId(commentId int64) (comment *model.Comment, err error) {
-	err = DB.Where(model.Comment{Id: commentId}).Find(&comment).Error
+	err = DB.Where(model.Comment{Id: commentId, IsDeleted: 0}).Find(&comment).Error
 	return comment, nil
 }
 
@@ -14,4 +16,10 @@ func CreateComment(comment *model.Comment) (*model.Comment, error) {
 
 func DeleteComment(commentId int64) error {
 	return DB.Where(model.Comment{Id: commentId}).Updates(model.Comment{IsDeleted: 1}).Error
+}
+
+func QueryCommentsByVideoId(videoId int64) ([]*model.Comment, error) {
+	var comments []*model.Comment
+	err := DB.Where(model.Comment{VideoId: videoId}).Find(&comments).Error
+	return comments, err
 }
--- biz/dal/mysql/follower.go
+++ biz/dal/mysql/follower.go
@@ -59,3 +59,12 @@ func QueryFollowSelf(uid int64) (int64, error) {
 	}
 	return total, nil
 }
+
+// QueryIfFollowSomeone 查询是否存在关注的关系
+func QueryIfFollowSomeone(commentCreatorId int64, userID int64) (int64, error) {
+	db := DB.Model(model.Follower{})
+	db.Where(model.Follower{ToUserUid: commentCreatorId, UserUid: userID})
+	var total int64
+	err := db.Count(&total).Error
+	return total, err
+}
