f987c5b68afe01f65bfd9cd4ff28b7e19de3969f
DoudiNCer <doudi@outlook.lv>
2023-02-18 09:37:00 +0800

    fix: 合并芮芮的 PR 时出现的错误

--- biz/dal/mysql/follower.go
+++ biz/dal/mysql/follower.go
@@ -65,3 +65,12 @@ func QueryForCheck(uid, toUid int64) (*model.Follower, int64, error) {
 
 	return res, total, nil
 }
+
+// QueryIfFollowSomeone 查询是否存在关注的关系
+func QueryIfFollowSomeone(commentCreatorId int64, userID int64) (int64, error) {
+	db := DB.Model(model.Follower{})
+	db.Where(model.Follower{ToUserUid: commentCreatorId, UserUid: userID})
+	var total int64
+	err := db.Count(&total).Error
+	return total, err
+}
