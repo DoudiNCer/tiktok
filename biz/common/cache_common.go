a0917f27fe9682aa3b260b4218db945b22214ee8
tzihklearn <1922941549@qq.com>
2023-02-23 23:03:46 +0800

    feat:完成了一部分缓存

--- biz/common/cache_common.go
+++ biz/common/cache_common.go
@@ -8,6 +8,11 @@ import (
 var CacheManager = cache.New(24*time.Hour, time.Duration(time.Monday))
 var KeyAddUser = "key_add_user"
 var KeyAddFriend = "key_add_friend"
+var KeyVideoFavorite = "key_video"
+var KeyUserFavorite = "key_favorite_creator"
+var KeyFollowIs = "key_follow_is"
+var KeyFavoriteIs = "key_favorite_is"
+var KeyVideoCreator = "key_video_creator"
 
 // DeleteUserReferTo 改变了user信息的接口都需要执行此操作
 // 例如：赞，关注，更新用户信息
